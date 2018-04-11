b4208f9be8e1bb7ff1584afb2aa6106f7292f2ef
spacejack <spacejack@gmail.com>
2018-04-11 17:18:18 -0400

    Demonstrate change and animate hooks in demo

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import PanelSlider from './panel-slider'
 
+function $e (id: string) {
+	return document.getElementById(id) as HTMLElement
+}
+
 const slider = PanelSlider({
 	element: document.querySelector('.panel-set') as HTMLElement,
 	numPanels: 3,
 	initialPanel: 0
 })
 
+const elId = $e('panelId')
+const elPos = $e('panelPos')
+
 slider.on('change', panelId => {
-	console.log("Changed to panel " + panelId)
+	//console.log("Changed to panel " + panelId)
+	elId.textContent = String(panelId)
+})
+
+slider.on('animate', panelPos => {
+	elPos.textContent = panelPos.toFixed(2)
 })
